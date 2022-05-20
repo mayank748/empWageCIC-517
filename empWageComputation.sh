0cf68bec8412a05c2ee84c087404c90d5f2a8138
Mayank <smayank748@gmail.com>
2022-05-20 09:51:09 +0530

    [Mayank]implemetation of emp attandence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,19 @@
+#! /bin/bash -x
+
+IS_PRESENT=1
+IS_ABSENT=0
+randomValue=0
+presentAbsent=-1
+
+
 echo "Welcome to emp wage"
+
+randomValue=$((RANDOM))
+presentAbsent=$(($randomValue%2))
+
+if [ $presentAbsent -eq $IS_PRESENT ]
+then
+	echo "Emp present"
+else
+	echo "Emp absent"
+fi
