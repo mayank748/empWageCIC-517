964bbebb58598341ddc0217890321ad696ab8769
Mayank <smayank748@gmail.com>
2022-05-20 09:59:05 +0530

    [Mayank]add daily wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,7 +4,9 @@ IS_PRESENT=1
 IS_ABSENT=0
 randomValue=0
 presentAbsent=-1
-
+TOTAL_WORKING_HOUR=8
+WAGE_PER_HOUR_FULL_DAY=20
+totalWage=0
 
 echo "Welcome to emp wage"
 
@@ -13,7 +15,10 @@ presentAbsent=$(($randomValue%2))
 
 if [ $presentAbsent -eq $IS_PRESENT ]
 then
-	echo "Emp present"
+        echo "Emp present"
+        totalWage=$(($TOTAL_WORKING_HOUR*$WAGE_PER_HOUR_FULL_DAY))
 else
-	echo "Emp absent"
+        echo "Emp absent"
 fi
+
+echo $totalWage
